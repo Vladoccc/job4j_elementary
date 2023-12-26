fd1f4c9e5af2d35663790127b156357e0dfb5438
vladoccc <vladrma777@gmail.com>
2023-12-26 10:11:11 +0300

    6. Checkstyle [#4123]

---
+++ src/test/java/ru/job4j/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package ru.job4j.ru.job4j.calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int sixDivtwo = six / two;
+        int fiveMinustwo = five - two;
+        int fourTimetwo = four * two;
+        int onePlustwo = one + two;
+        System.out.println(sixDivtwo);
+        System.out.println(fiveMinustwo);
+        System.out.println(fourTimetwo);
+        System.out.println(onePlustwo);
+    }
+}
