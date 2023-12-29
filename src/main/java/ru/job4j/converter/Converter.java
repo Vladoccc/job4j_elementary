2f071779716f1cebfa16c8b24c153ed58f5b3202
vladoccc <vladrma777@gmail.com>
2023-12-29 12:02:40 +0300

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -12,23 +12,35 @@ public class Converter {
     }
 
     public static float euroToDollar(float value) {
-        float result  = value * 0.85F;
+        float result  = value * 1.16F;
         return result;
     }
 
     public static float dollarToEuro(float value) {
-        float result = value * 1.16F;
+        float result = value * 0.85F;
         return result;
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(70);
-        float dollar = Converter.rubleToDollar(60);
-        float euro1 = Converter.euroToDollar(10);
-        float dollar1 = Converter.dollarToEuro(10);
-        System.out.println("70 rubles are " + euro + " euro. ");
-        System.out.println("60 rubles are " + dollar + " dollar. ");
-        System.out.println("10 euro are " + dollar1 + " dollar. ");
-        System.out.println("10 dollar are " + euro1 + " euro. ");
+        float input1 = 140;
+        float expected1 = 2;
+        float outpud1 = Converter.rubleToEuro(input1);
+        boolean passed1 = expected1 == outpud1;
+        float input2 = 120;
+        float expected2 = 2;
+        float outpud2 = Converter.rubleToDollar(input2);
+        boolean passed2 = expected2 == outpud2;
+        float input3 = 100;
+        float expected3 = 116;
+        float outpud3 = Converter.euroToDollar(input3);
+        boolean passed3 = expected3 == outpud3;
+        float input4 = 100;
+        float expected4 = 85;
+        float outpud4 = Converter.dollarToEuro(input4);
+        boolean passed4 = expected4 == outpud4;
+        System.out.println("140 rubles are 2 euro. Test result : " + passed1);
+        System.out.println("120 rubles are 2 dollar. Test result : " + passed2);
+        System.out.println("100 euro are 116 dollar. Test result : " + passed3);
+        System.out.println("100 dollar are 85 euro. Test result : " + passed4);
     }
 }
