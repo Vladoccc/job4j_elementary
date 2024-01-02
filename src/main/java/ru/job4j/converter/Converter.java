43292a7bd7d38c4f6c4ee6273614112b2f1bfa81
vladoccc <vladrma777@gmail.com>
2024-01-02 17:18:50 +0300

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -26,21 +26,20 @@ public class Converter {
         float expected1 = 2;
         float outpud1 = Converter.rubleToEuro(input1);
         boolean passed1 = expected1 == outpud1;
-        float input2 = 120;
-        float expected2 = 2;
-        float outpud2 = Converter.rubleToDollar(input2);
-        boolean passed2 = expected2 == outpud2;
-        float input3 = 100;
-        float expected3 = 116;
-        float outpud3 = Converter.euroToDollar(input3);
-        boolean passed3 = expected3 == outpud3;
-        float input4 = 100;
-        float expected4 = 85;
-        float outpud4 = Converter.dollarToEuro(input4);
-        boolean passed4 = expected4 == outpud4;
         System.out.println("140 rubles are 2 euro. Test result : " + passed1);
-        System.out.println("120 rubles are 2 dollar. Test result : " + passed2);
-        System.out.println("100 euro are 116 dollar. Test result : " + passed3);
-        System.out.println("100 dollar are 85 euro. Test result : " + passed4);
+        input1 = 120;
+        outpud1 = Converter.rubleToDollar(input1);
+        passed1 = expected1 == outpud1;
+        System.out.println("120 rubles are 2 dollar. Test result : " + passed1);
+        input1 = 100;
+        expected1 = 116;
+        outpud1 = Converter.euroToDollar(input1);
+        passed1 = expected1 == outpud1;
+        System.out.println("100 euro are 116 dollar. Test result : " + passed1);
+        input1 = 100;
+        expected1 = 85;
+        outpud1 = Converter.dollarToEuro(input1);
+        passed1 = expected1 == outpud1;
+        System.out.println("100 dollar are 85 euro. Test result : " + passed1);
     }
 }
---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.converter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConverterTest {
+
+    @Test
+    void rubleToEuro() {
+    }
+
+    @Test
+    void rubleToDollar() {
+    }
+
+    @Test
+    void euroToDollar() {
+    }
+
+    @Test
+    void dollarToEuro() {
+    }
+
+    @Test
+    void main() {
+    }
+}
