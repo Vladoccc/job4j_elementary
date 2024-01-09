d692ee82b8d397f1143a812fca15b1ebe7fccb2b
vladoccc <vladrma777@gmail.com>
2024-01-09 12:28:10 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -3,20 +3,29 @@ package ru.job4j.loop;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
-        if (start > finish) {
-            return sum;
-        }
         for (sum = start; start <= finish; start++) {
             sum = sum + start;
-         }
+        }
+        return sum;
+    }
+
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+                sum = sum + i;
+            }
+        }
         return sum;
     }
 
     public static void main(String[] args) {
-        System.out.println(sum(0, 5));
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
-        System.out.println(sum(5, 0));
+        System.out.println(sumByEven(0, 10));
+        System.out.println(sumByEven(3, 8));
+        System.out.println(sumByEven(1, 1));
+        System.out.println(sumByEven(-3, 6));
     }
 }
--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -7,19 +7,10 @@ import static org.assertj.core.api.Assertions.*;
 class CounterTest {
 
     @Test
-    void when0To5Then15() {
-        int start = 0;
-        int finish = 5;
-        int result  = Counter.sum(start, finish);
-        int expected = 15;
-        assertThat(result).isEqualTo(expected);
-    }
-
-    @Test
     void when0To10Then55() {
         int start = 0;
         int finish = 10;
-        int result  = Counter.sum(start, finish);
+        int result = Counter.sum(start, finish);
         int expected = 55;
         assertThat(result).isEqualTo(expected);
     }
@@ -28,7 +19,7 @@ class CounterTest {
     void when3To8Then36() {
         int start = 3;
         int finish = 8;
-        int result  = Counter.sum(start, finish);
+        int result = Counter.sum(start, finish);
         int expected = 36;
         assertThat(result).isEqualTo(expected);
     }
@@ -37,17 +28,44 @@ class CounterTest {
     void when1To1Then2() {
         int start = 1;
         int finish = 1;
-        int result  = Counter.sum(start, finish);
+        int result = Counter.sum(start, finish);
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    void when5To0Then0() {
-        int start = 5;
+    void whenSumEvenNumbersFrom0To10Then30() {
+        int start = 0;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFrom3To8Then18() {
+        int start = 3;
+        int finish = 8;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 18;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFrom1To1Then0() {
+        int start = 1;
         int finish = 1;
-        int result  = Counter.sum(start, finish);
+        int result = Counter.sumByEven(start, finish);
         int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromMinus3To6Then10() {
+        int start = -3;
+        int finish = 6;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 10;
         assertThat(result).isEqualTo(expected);
     }
 }
