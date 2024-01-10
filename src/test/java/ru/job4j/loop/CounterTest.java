29f55310eda01cc944f05335c283c58edcd3c111
vladoccc <vladrma777@gmail.com>
2024-01-10 11:02:13 +0300

    5.0.1. Сумма чисел. [#226416 #471751]

--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -7,6 +7,15 @@ import static org.assertj.core.api.Assertions.*;
 class CounterTest {
 
     @Test
+    void when0To5Then15() {
+        int start = 0;
+        int finish = 5;
+        int result = Counter.sum(start, finish);
+        int expected = 15;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
     void when0To10Then55() {
         int start = 0;
         int finish = 10;
@@ -16,11 +25,11 @@ class CounterTest {
     }
 
     @Test
-    void when3To8Then36() {
+    void when3To8Then33() {
         int start = 3;
         int finish = 8;
         int result = Counter.sum(start, finish);
-        int expected = 36;
+        int expected = 33;
         assertThat(result).isEqualTo(expected);
     }
 
@@ -29,43 +38,16 @@ class CounterTest {
         int start = 1;
         int finish = 1;
         int result = Counter.sum(start, finish);
-        int expected = 2;
+        int expected = 1;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    void whenSumEvenNumbersFrom0To10Then30() {
-        int start = 0;
-        int finish = 10;
-        int result = Counter.sumByEven(start, finish);
-        int expected = 30;
-        assertThat(result).isEqualTo(expected);
-    }
-
-    @Test
-    void whenSumEvenNumbersFrom3To8Then18() {
-        int start = 3;
-        int finish = 8;
-        int result = Counter.sumByEven(start, finish);
-        int expected = 18;
-        assertThat(result).isEqualTo(expected);
-    }
-
-    @Test
-    void whenSumEvenNumbersFrom1To1Then0() {
-        int start = 1;
+    void when5To0Then0() {
+        int start = 5;
         int finish = 1;
-        int result = Counter.sumByEven(start, finish);
+        int result = Counter.sum(start, finish);
         int expected = 0;
-        assertThat(result).isEqualTo(expected);
-    }
-
-    @Test
-    void whenSumEvenNumbersFromMinus3To6Then10() {
-        int start = -3;
-        int finish = 6;
-        int result = Counter.sumByEven(start, finish);
-        int expected = 10;
         assertThat(result).isEqualTo(expected);
     }
 }
